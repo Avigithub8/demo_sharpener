8bbe110255d6e064906d0ba0984542062883bdf9
Avigithub8 <sharma.avi844@gmail.com>
2023-11-06 15:38:45 +0530

    new index file and dom

---
+++ dom.js
@@ -0,0 +1,5 @@
+let item=document.getElementsByClassName('list-group-item');
+item[2].style.backgroundColor='green';
+
+let items=document.getElementsByClassName('list-group');
+items.style.fontWeight="bold";
